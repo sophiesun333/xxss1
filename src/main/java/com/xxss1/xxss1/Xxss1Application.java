602f2561efd9bb236b24462e30be6034855ddf9e
sophiesun333 <sunzhihuiyu@163.com>
2023-12-20 14:54:55 +0800

    debug

---
+++ src/main/java/com/xxss1/xxss1/Xxss1Application.java
@@ -0,0 +1,13 @@
+package com.xxss1.xxss1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xxss1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Xxss1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xxss1/xxss1/Xxss1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxss1.xxss1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xxss1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
