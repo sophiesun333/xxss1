5c72271848e0f6b4b3f9a06baa8fe7ceedf21c14
sophiesun333 <sunzhihuiyu@163.com>
2023-12-20 15:12:09 +0800

    OCT: add files

--- src/main/java/com/xxss1/xxss1/Xxss1Application.java
+++ src/main/java/com/xxss1/xxss1/Xxss1Application.java
@@ -3,6 +3,7 @@ package com.xxss1.xxss1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Xxss1Application {
 
